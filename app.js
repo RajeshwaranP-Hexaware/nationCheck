806ab80b8a6d1a59ec211563c3e4cccda9ee459c
Rajeshwaran <rajeshfederer2@gmail.com>
2018-09-13 18:24:30 +0530

    TRYING CONTEXTOUT

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express()
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.post('/', (req, res) => {
+    console.log('REQUSST', JSON.stringify(req.body));
+});
+
+app.listen(process.env.PORT || 5000, () => console.log('Example app listening on port 3000!'));
