7dc016fe2189947a20f9d03949461d66b9e28f07
Rajeshwaran <rajeshfederer2@gmail.com>
2018-09-13 18:33:26 +0530

    TRYING CONTEXTOUT

--- app.js
+++ app.js
@@ -9,6 +9,119 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 
 app.post('/', (req, res) => {
     console.log('REQUSST', JSON.stringify(req.body));
+    if(req.body.result.metadata.intentName == "addNationalId"){
+        let leave_details= [];
+        leave_details.push({
+            "optionInfo": {
+                "key": `ind`,
+                "synonyms": `India`
+            },
+            "title": `India`,
+            "description": `india country`
+        });
+        leave_details.push({
+            "optionInfo": {
+                "key": `eng`,
+                "synonyms": `england`
+            },
+            "title": `England`,
+            "description": `England`
+        });
+        let data = {
+            "speechMsg": "Test",
+            "title": "Test",
+            "itemValues": leave_details
+        };
+
+        return {
+          "messages": [
+              {
+                  "displayText": data.speechMsg,
+                  "platform": "google",
+                  "textToSpeech": data.speechMsg,
+                  "type": "simple_response"
+              },  {
+                  "items": data.itemValues,
+                  "platform": "google",
+                  "title": data.title,
+                  "type": "list_card"
+              }
+          ]
+      };
+    } else if(req.body.result.metadata.intentName == "addNationalId-type"){
+      console.log('FULL REQ IN addNationalId', JSON.stringify(req.body));
+      var country, nationalIdType="", nationalId ="", comment=""
+      for(var i=0;i<contexts.length;i++){
+        if(contexts[i].name === "actions_intent_option"){
+            console.log("HEY RAJESH", JSON.stringify(contexts[i]));
+            country = contexts[i].parameters.OPTION;
+            nationalIdType = contexts[i].parameters.nationalIdType;
+            nationalId = contexts[i].parameters.nationalId;
+            comment = contexts[i].parameters.comment;
+        }
+     }
+
+     if(nationalIdType == ""){
+
+      let leave_details= [];
+      leave_details.push({
+          "optionInfo": {
+              "key": `pan`,
+              "synonyms": `Pan`
+          },
+          "title": `Pan`,
+          "description": `Pan Card`
+      });
+      leave_details.push({
+          "optionInfo": {
+              "key": `aadhar`,
+              "synonyms": `Aadhar`
+          },
+          "title": `Aadhar`,
+          "description": `aadhar card`
+      });
+      let data = {
+          "speechMsg": "Test 1",
+          "title": "Test 1",
+          "itemValues": leave_details
+      };
+
+      return {
+          "contextOut": [
+              {
+                  "name": "actions_intent_option",
+                  "parameters": {
+                    "country": country,
+                    "country.original": country,
+                    "nationalIdType": nationalIdType,
+                    "nationalIdType.original": nationalIdType,
+                    "nationalId": nationalId,
+                    "nationalId.original": nationalId,
+                    "comment": comment,
+                    "comment.original": comment
+                  },
+                  "lifespan": 5
+                }
+          ],
+        "messages": [
+            {
+                "displayText": data.speechMsg,
+                "platform": "google",
+                "textToSpeech": data.speechMsg,
+                "type": "simple_response"
+            },  {
+                "items": data.itemValues,
+                "platform": "google",
+                "title": data.title,
+                "type": "list_card"
+            }
+        ]
+    };
+  }
+  } else{
+    console.log('JEJJEJE', req.body);
+  }
+
 });
 
 app.listen(process.env.PORT || 5000, () => console.log('Example app listening on port 3000!'));
